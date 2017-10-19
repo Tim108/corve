e59ece1ebd20af13656a858ee30fe1d6acf56ee0
Tim108 <tsonderen108@gmail.com>
2017-10-19 16:02:16 +0200

    added graceful stop method

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 import corve.Core;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.print("Type stop to stop the system and exit");
+
         Core c = new Core();
         Thread t = new Thread(c);
         t.start();
 
-        boolean stop = false;
-        while (!stop) {
-            System.out.print("Type stop to stop the system and exit");
-            String input = System.console().readLine();
-            if (input.equals("stop")) {
-                stop = true;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            if (sc.nextLine().equals("stop")) {
+                c.stop();
+                break;
             }
         }
-
-        c.stop();
     }
 
 
