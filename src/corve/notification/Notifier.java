23b02b915b9bf660b61f9d73a527b24c589311a8
Tim108 <tsonderen108@gmail.com>
2018-02-19 10:59:11 +0100

    added maven framework

--- src/corve/notification/Notifier.java
+++
@@ -1,9 +0,0 @@
-package corve.notification;
-
-import java.sql.Timestamp;
-
-public interface Notifier {
-    void notifyOfAssignment(String email, String roomName, Timestamp endDate, String choreName, String code);
-    void notifyOfFine(String email, String roomName, String choreName);
-    void notifyOfReward(String email, String roomName, String choreName);
-}
